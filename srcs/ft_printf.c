1125218460ef647c0b44813a4b3df694ffbd66f8
alagache <alagache@student.42.fr>
2019-10-28 13:38:29 +0100

    loop

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: plagache <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 15:58:10 by plagache          #+#    #+#             */
-/*   Updated: 2019/10/24 16:27:34 by alagache         ###   ########.fr       */
+/*   Updated: 2019/10/28 13:37:56 by alagache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int			ft_printf(const char *format, ...)
 	int		written;
 
 	va_start(args, format);
-	written = loop_conv(1, format, &args, 0);
+	written = loop(1, format, &args, 0);
 	va_end(args);
 	return (written);
 }
